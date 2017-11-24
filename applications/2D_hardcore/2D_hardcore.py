e36148f56cacd4f87aa955bea14e92a2c4c5dfcf
Shusuke KASAMATSU <s.kasamatsu@gmail.com>
2017-11-24 15:05:58 +0900

    animation works in 2D hard disc

--- applications/2D_hardcore/2D_hardcore.py
+++ applications/2D_hardcore/2D_hardcore.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random as rand
 import sys
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
 
 from mc import CanonicalMonteCarlo, grid_1D
 from model_setup import *
@@ -8,21 +11,37 @@ from model_setup import *
 
 if __name__ == "__main__":
     L = 1.0
-    rdisc = 1.0/14.0
-    Ndisc = 20
-    dr = 0.01
-    maxstep = rdisc/2
+    nu = 5
+    d = 1.0/14.0
+    rdisc = d*(1.0-2.0**(nu-8))*0.5
+    Ndisc = 224
+    dr = 0.001
+    maxstep = d - rdisc*2.0
     kT = 1
     
-    grid = grid_1D(dr, dr, L/2.0)
+    grid = grid_1D(dr, dr, L/4.0)
     config = HC_2D_config(L, Ndisc, rdisc)
     model = HC_2D(maxstep)
-    config.prepare_ordered()
+    config.prepare_metropolis()
     print(config.coors)
     print(g_r(config, grid))
     calc = CanonicalMonteCarlo(model, kT, config, grid=grid)
-    plot_fig(calc, 10000)
-    #calc.run(10000, observefunc=observables)
+    #plot_fig(calc, 100000)
+    colors = ["blue", "red", "green", "magenta"]
+    obs = 0
+    for i in range(4):
+
+        calc.config.prepare_metropolis()
+        obs_init = calc.run(10000)
+        obs_now = calc.run(10000, sample_frequency=1,observefunc=observables)
+        plt.plot(grid.x, obs_now, "o", color=colors[i])
+        obs += obs_now
+
+    cs = CubicSpline(grid.x, obs/4)
+    x_cs = np.arange(rdisc*2.0, grid.x[-1], 0.00001)
+    plt.plot(x_cs, cs(x_cs), label='average')
+    #plt.plot(grid.x, obs/4)
+    plt.show()
     #print(config.coors)
     
     
