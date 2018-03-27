49eab96f4adc7fc20071d7c0aeccfd6b5655a79f
skasamatsu <s.kasamatsu@gmail.com>
2018-03-27 14:44:29 +0900

    decided to use observer class object instead of observer function for logging

--- py_mc/mc.py
+++ py_mc/mc.py
@@ -48,9 +48,74 @@ class grid_1D:
     def __init__(self, dx, minx, maxx):
         self.dx = dx
         self.x = np.arange(minx, maxx, dx)
-    
-class CanonicalMonteCarlo:
 
+def obs_encode(*args):
+    nargs = np.array([len(args)])
+    args_length_list = []
+    obs_array = np.array([])
+    for arg in args:
+        # Inelegant way to make everything a 1D array
+        arg = np.array([arg])
+        arg = arg.ravel()
+        obs_array = np.concatenate((obs_array, arg))
+        args_length_list.append(len(arg))
+    args_length_array = np.array(args_length_list)
+    args_info = np.concatenate((nargs, args_length_array))
+    return args_info, obs_array
+
+def obs_decode(args_info, obs_array):
+    nargs = args_info[0]
+    args_length_array = args_info[1:nargs+1]
+    args = []
+    idx = 0
+    for i in range(nargs):
+        length = args_length_array[i]
+        if length == 1:
+            args.append(obs_array[idx])
+        else:
+            args.append(obs_array[idx:idx+length])
+        idx += length
+    return args
+
+def make_observefunc(logfunc,*multiDfuncs):
+    def observefunc(calc_state, outputfi):
+        obs_log = logfunc(calc_state)
+        outputfi.write(str(calc_state.kT)+"\t")
+        if hasattr(obs_log, '__getitem__'):
+            outputfi.write("\t".join([str(observable) for observable in obs_log])+"\n")
+        else:
+            outputfi.write(str(obs_log)+"\n")
+        obs_ND = []
+        for func in multiDfuncs:
+            obs_ND.append(func(calc_state))
+        return obs_encode(*obs_log, *obs_ND)
+    return observefunc
+
+        
+class observer_base:
+    def logfunc(self, calc_state):
+        return calc_state.energy,
+    def savefuncs(self, calc_state):
+        return None
+    def observe(self, calc_state, outputfi):
+        obs_log = self.logfunc(calc_state)
+        outputfi.write(str(calc_state.kT)+"\t")
+        if isinstance(obs_log, tuple):
+            outputfi.write("\t".join([str(observable) for observable in obs_log])+"\n")
+        else:
+            outputfi.write(str(obs_log)+"\n")
+        obs_ND = []
+        obs_save = self.savefuncs(calc_state)
+        if obs_save != None:
+            if isinstance(obs_save, tuple):
+                for obs in obs_save:
+                    obs_ND.append(obs)
+
+            else:
+                obs_ND.append(obs_save)
+        return obs_encode(*obs_log, *obs_ND)
+        
+class CanonicalMonteCarlo:
     def __init__(self, model, kT, config, grid=0):
         self.model = model
         self.config = config
@@ -73,7 +138,7 @@ class CanonicalMonteCarlo:
                 self.energy += dE
                 #print "trial accepted"
 
-    def run(self, nsteps, sample_frequency=0, observefunc=lambda *args: (None,None)):
+    def run(self, nsteps, sample_frequency=0, observer=observer_base()):
         if not sample_frequency:
             sample_frequency = float("inf")
             
@@ -81,16 +146,16 @@ class CanonicalMonteCarlo:
         nsample = 0
         self.energy = self.model.energy(self.config)
         output = open("obs.dat", "a")
-        if hasattr(observefunc(self,open(os.devnull,"w"))[1],'__add__'):
+        if hasattr(observer.observe(self,open(os.devnull,"w"))[1],'__add__'):
             observe = True
         else:
             observe = False
             
-        for i in range(nsteps):
+        for i in range(1,nsteps+1):
             self.MCstep()
             sys.stdout.flush()
             if i%sample_frequency == 0 and observe:
-                args_info, obs_step = observefunc(self,output)
+                args_info, obs_step = observer.observe(self,output)
                 observables += obs_step
                 nsample += 1
         if nsample > 0:
@@ -204,46 +269,5 @@ class TemperatureReplicaExchange:
         #self.accept_count = 0
 
 
-def obs_encode(*args):
-    nargs = np.array([len(args)])
-    args_length_list = []
-    obs_array = np.array([])
-    for arg in args:
-        # Inelegant way to make everything a 1D array
-        arg = np.array([arg])
-        arg = arg.ravel()
-        obs_array = np.concatenate((obs_array, arg))
-        args_length_list.append(len(arg))
-    args_length_array = np.array(args_length_list)
-    args_info = np.concatenate((nargs, args_length_array))
-    return args_info, obs_array
-
-def obs_decode(args_info, obs_array):
-    nargs = args_info[0]
-    args_length_array = args_info[1:nargs+1]
-    args = []
-    idx = 0
-    for i in range(nargs):
-        length = args_length_array[i]
-        if length == 1:
-            args.append(obs_array[idx])
-        else:
-            args.append(obs_array[idx:idx+length])
-        idx += length
-    return args
-
-def make_observefunc(logfunc,*multiDfuncs):
-    def observefunc(calc_state, outputfi):
-        obs_log = logfunc(calc_state)
-        outputfi.write(str(calc_state.kT)+"\t")
-        if hasattr(obs_log, '__getitem__'):
-            outputfi.write("\t".join([str(observable) for observable in obs_log])+"\n")
-        else:
-            outputfi.write(str(obs_log)+"\n")
-        obs_ND = []
-        for func in multiDfuncs:
-            obs_ND.append(func(calc_state))
-        return obs_encode(*obs_log, *obs_ND)
-    return observefunc
             
             
